52ae2bc6a2606a094771f045de89a921bf14dece
Екатерина Зенина <zeninaekaterin@gmail.com>
2022-03-06 10:53:40 +0300

    update

--- src/test/java/ShouldRegByAccountTest.java
+++ src/test/java/ShouldRegByAccountTest.java
@@ -25,22 +25,22 @@ public class ShouldRegByAccountTest {
     }
 
 
-    String planingDate = DataGenerator.generateDate(3);
+    String planingDate = DataGenerator.generateDate(1);
     @Test
     public void shouldRegByAccount() {
         open("http://localhost:9999");
         Configuration.holdBrowserOpen = true;
         $("[data-test-id='city'] input").setValue(DataGenerator.getCity());
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(DataGenerator.generateDate(3));
+        $("[data-test-id='date'] input").setValue(DataGenerator.generateDate(1));
         $("[data-test-id='name'] input").setValue(DataGenerator.getName());
         $x("//*[@name='phone']").setValue(DataGenerator.getPhone());
         $(".checkbox__box").click();
         $("button.button").click();
-        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + DataGenerator.generateDate(3)), Duration.ofSeconds(15)).shouldBe(visible);
+        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + DataGenerator.generateDate(1)), Duration.ofSeconds(15)).shouldBe(visible);
 
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(DataGenerator.generateDate(3));
+        $("[data-test-id='date'] .input__control").setValue(DataGenerator.generateDate(1));
         $$(".button").find(text("Запланировать")).click();
         $$(".button__text").find(text("Перепланировать")).click();
         $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + DataGenerator.generateDate(3)), Duration.ofSeconds(15)).shouldBe(visible);
